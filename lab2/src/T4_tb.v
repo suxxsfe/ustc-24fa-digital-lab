f8aec42495a3103ef431ab1a0e522db6591dba6d
suxxsfe <60848652+suxxsfe@users.noreply.github.com>
2024-10-10 16:57:32 +0800

    fix T4_tb

--- lab2/src/T4_tb.v
+++ lab2/src/T4_tb.v
@@ -13,23 +13,28 @@ module T4_tb();
 	end
 	always @(posedge clk) begin
 		if(nowBus == 0) begin
-			bus = bus+1;
-			a = 1;
-			nowBus = bus;
-			case(bus)
-				16'd1: nowA = 1;
-				16'd2: nowA = 1;
-				16'd3: nowA = 1;
-				16'd4: nowA = 3;
-				16'd5: nowA = 2;
-				default: nowA = 1;
+			bus <= bus+1;
+			a <= ~zero;
+			nowBus <= bus;
+			case(bus+1)
+				16'd1: nowA <= 0;
+				16'd2: nowA <= 0;
+				16'd3: nowA <= 0;
+				16'd4: nowA <= 2;
+				16'd5: nowA <= 1;
+				default: nowA <= 0;
 			endcase
 		end
-		if(nowA == 0 || zero) begin
-			a = 0;
+		else if(nowA == 0) begin
+			nowBus <= nowBus-1;
+			nowA <= 0;
+			a <= 0;
 		end
-		nowBus = nowBus-1;
-		nowA = nowA-1;
+		else begin
+			nowBus <= nowBus-1;
+			nowA <= nowA-1;
+			a <= ~zero;
+		end
 	end
 	
 	always begin
