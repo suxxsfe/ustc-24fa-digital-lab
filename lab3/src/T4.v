665c4fd6b5c679b5e805f83634f0a2c5f3e1393a
suxxsfe <suxxsfe@gmail.com>
2024-10-25 13:13:31 +0800

    fix

--- lab3/src/T4.v
+++ lab3/src/T4.v
@@ -10,6 +10,7 @@ module T4(
 
 reg [31:0] counter;
 reg [2:0] seg_id;
+reg [2:0] now_id;
 parameter TIME_CNT = 250_000;
 
 always @(posedge clk) begin
@@ -29,18 +30,24 @@ end
 always @(posedge clk) begin
 	if(rst) begin
 		seg_id <= 0;
+		now_id <= 0;
 	end
     else if(counter >= TIME_CNT) begin
-		seg_id <= seg_id+3'd1;
+		now_id <= now_id+3'd1;
+		if(now_id == 0 || output_valid[now_id]) begin
+		    seg_id <= now_id;
+		end
+		else begin
+		    seg_id <= 0;
+		end
 	end
 end
 
 always @(*) begin
     seg_data = 0;
     seg_an = seg_id;
-	seg_data = output_valid[seg_id] || seg_id == 0 ?
-				{output_data[seg_id*4+3], output_data[seg_id*4+2],
-				 output_data[seg_id*4+1], output_data[seg_id*4]} : 0;
+	seg_data = {output_data[seg_id*4+3], output_data[seg_id*4+2],
+				 output_data[seg_id*4+1], output_data[seg_id*4]};
 end
 
 endmodule
