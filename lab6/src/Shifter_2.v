a65cd4a6931d3c22d78ebeddc1311a2186a05fc8
suxxsfe <60848652+suxxsfe@users.noreply.github.com>
2024-10-30 00:11:42 +0800

    fix: trans tab to spaces

--- lab6/src/Shifter_2.v
+++ lab6/src/Shifter_2.v
@@ -5,30 +5,30 @@ module Shifter_2(
     output     reg          [31 : 0]        res2        //算术右移
 );
 
-	always @(*) begin
-		res1 = src0;
-		res2 = src0;
-	
-		if(src1[0]) begin
-			res1 = {1'b0, res1[31:1]};
-			res2 = {{1{res2[31]}}, res2[31:1]};
-		end
-		if(src1[1]) begin
-			res1 = {2'b0, res1[31:2]};
-			res2 = {{2{res2[31]}}, res2[31:2]};
-		end
-		if(src1[2]) begin
-			res1 = {4'b0, res1[31:4]};
-			res2 = {{4{res2[31]}}, res2[31:4]};
-		end
-		if(src1[3]) begin
-			res1 = {8'b0, res1[31:8]};
-			res2 = {{8{res2[31]}}, res2[31:8]};
-		end
-		if(src1[4]) begin
-			res1 = {16'b0, res1[31:16]};
-			res2 = {{16{res2[31]}}, res2[31:16]};
-		end
-	end
+    always @(*) begin
+        res1 = src0;
+        res2 = src0;
+    
+        if(src1[0]) begin
+            res1 = {1'b0, res1[31:1]};
+            res2 = {{1{res2[31]}}, res2[31:1]};
+        end
+        if(src1[1]) begin
+            res1 = {2'b0, res1[31:2]};
+            res2 = {{2{res2[31]}}, res2[31:2]};
+        end
+        if(src1[2]) begin
+            res1 = {4'b0, res1[31:4]};
+            res2 = {{4{res2[31]}}, res2[31:4]};
+        end
+        if(src1[3]) begin
+            res1 = {8'b0, res1[31:8]};
+            res2 = {{8{res2[31]}}, res2[31:8]};
+        end
+        if(src1[4]) begin
+            res1 = {16'b0, res1[31:16]};
+            res2 = {{16{res2[31]}}, res2[31:16]};
+        end
+    end
 
 endmodule
