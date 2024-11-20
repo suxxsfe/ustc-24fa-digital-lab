ae992c50fb05f55b69d6d56fe2c6e5d12217ad01
suxxsfe <60848652+suxxsfe@users.noreply.github.com>
2024-11-21 01:25:08 +0800

    fix

--- lab7/src/Adder8.v
+++ lab7/src/Adder8.v
@@ -13,13 +13,24 @@ module Adder8 #(parameter WIDTH = 64)(
     genvar i;
     generate
         for (i = 0; i < N; i = i + 1) begin
+            wire [8:0] out;
             Adder_LookAhead8 adder(
-                .a(a[i*8 +: 8]),
-                .b(b[i*8 +: 8]),
+                .a(i*8+7 < WIDTH ? a[i*8+7 : i*8] :
+                                   {{(8*i+8-WIDTH){1'b0}}, a[WIDTH-1 : i*8]}),
+                .b(i*8+7 < WIDTH ? b[i*8+7 : i*8] :
+                                   {{(8*i+8-WIDTH){1'b0}}, b[WIDTH-1 : i*8]}),
                 .ci(cmid[i]),
-                .s(s[i*8 +: 8]),
-                .co(cmid[i+1])
+                .s(out),
+                .co(out[8])
             );
+            if(i*8+7 < WIDTH) begin
+                assign s[i*8+7 : i*8] = out[7 : 0];
+                assign cmid[i+1] = out[8];
+            end
+            else begin
+                assign s[WIDTH-1 : i*8] = out[WIDTH-8*i-1 : 0];
+                assign cmid[i+1] = out[WIDTH-8*i];
+            end
         end
     endgenerate
 
