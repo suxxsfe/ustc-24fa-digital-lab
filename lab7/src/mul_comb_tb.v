f9ee2b78a8bcc283b5529fb4836979f8ed2c9086
suxxsfe <60848652+suxxsfe@users.noreply.github.com>
2024-11-22 00:53:49 +0800

    mul_comb tb

---
+++ lab7/src/mul_comb_tb.v
@@ -0,0 +1,45 @@
+module mul_comb_tb #(
+    parameter WIDTH = 8
+) ();
+reg  [WIDTH-1:0]    a, b;
+reg                 rst, clk, start;
+wire [2*WIDTH-1:0]  res;
+wire                finish;
+integer             seed;
+
+initial begin
+    clk = 0;
+    seed = 2024; // 种子值
+    forever begin
+        #5 clk = ~clk;
+    end
+end
+
+initial begin
+    rst = 1;
+    start = 0;
+    #20;
+    rst = 0;
+    #20;
+    repeat (5) begin
+        a = $random(seed);          
+        b = $random(seed + 1);      
+        start = 1;
+        #20 start = 0;
+        #380;
+    end
+    $finish;
+end
+
+mul_comb mul2(
+    .a          (a),
+    .b          (b),
+    .res        (res)
+);
+
+reg [2*WIDTH-1 : 0] correct;
+always @(*) begin
+    correct = {{WIDTH{1'b0}}, a} * b;
+end
+
+endmodule
