89408ede5575805adcddf6c4c450adb167b61ace
suxxsfe <60848652+suxxsfe@users.noreply.github.com>
2024-12-05 18:39:41 +0800

    test

---
+++ lab8/src/Uart_test.v
@@ -0,0 +1,8 @@
+module Uart_test(
+    input                   [ 0 : 0]            uart_din,
+    output                  [ 0 : 0]            uart_dout
+);
+
+    assign uart_dout = uart_din;
+    
+endmodule
